e673bebea62adfc831b24f06e58bdbe14f757928
JovanChristian <jovaness2005@gmail.com>
2025-05-08 13:04:12 +0700

    bikin pages dulu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
-
+import Home from './containers/home';
+import About from './containers/about';
+import Resume from './containers/resume';
+import Skills from './containers/skills';
+import Portofolio from './containers/portofolio';
+import Contact from './containers/contact';
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className='App'>
-      
+      {/* particles js */}
+
+      {/* navbar */}
+
+      {/* main page content */}
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route index path='/about' element={<About />} />
+        <Route index path='/resume' element={<Resume />} />
+        <Route index path='/skills' element={<Skills />} />
+        <Route index path='/portofolio' element={<Portofolio />} />
+        <Route index path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/containers/portofolio/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+const Portofolio=()=>{
+    return(
+        <div>
+            Portofolio Page
+        </div>
+    )
+}
+export default Portofolio;
