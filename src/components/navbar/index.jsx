3233f522c3379f40577b59dfd0098a58785d7123
JovanChristian <jovaness2005@gmail.com>
2025-05-13 14:52:16 +0700

    input logo

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,49 +1,48 @@
-import React from "react";
-import {FaReact} from 'react-icons/fa';
-import Home from "../../containers/home";
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { FaBars, FaReact } from 'react-icons/fa';
+import { HiX } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 const data = [
-    {
-        label : 'Home',
-        to : '/'
-    }, 
-    {
-        label : 'About Me', 
-        to : '/about'
-    }, 
-    {
-        label : 'Skills', 
-        to : '/skills'
-    }, 
-    {
-        label : 'Resume', 
-        to : '/resume'
-    }, 
-    {
-        label : 'Portofolio',
-        to : '/portofolio'
-    }, 
-    {
-        label : 'Contact', 
-        to : '/contact'
-    }
-]
+    { label: 'Home', to: '/' },
+    { label: 'About Me', to: '/about' },
+    { label: 'Skills', to: '/skills' },
+    { label: 'Resume', to: '/resume' },
+    { label: 'Portofolio', to: '/portofolio' },
+    { label: 'Contact', to: '/contact' }
+];
 
-const Navbar=()=>{
-    return(
+const Navbar = () => {
+    const [toggleIcon, setToggleIcon] = useState(false);
+    const handleToggleIcon = () => setToggleIcon(!toggleIcon);
+
+    return (
         <div>
             <nav className="navbar">
                 <div className="navbar_container">
-
-                    <Link to={'/'} className="navbar_container_logo">
-                        <FaReact size={30}/>
+                    <Link to='/' className="navbar_container_logo">
+                        <img
+                            src="/public/logo_portofolio_no_background"
+                            alt="JCE Logo"
+                            style={{ height: "40px" }}
+                        />
                     </Link>
+                </div>
+                <ul className="navbar_container_menu">
+                    {data.map((item, key) => (
+                        <li key={key} className="navbar_container_menu_item">
+                            <Link className="navbar_container_menu_item_links" to={item.to}>
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <div className="nav-icon" onClick={handleToggleIcon}>
+                    {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
                 </div>
             </nav>
         </div>
-    )
-
-}
+    );
+};
 
 export default Navbar;
