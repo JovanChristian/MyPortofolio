18a9dea0ff33ac574d3e626d7519d881e7a3f449
JovanChristian <jovaness2005@gmail.com>
2025-05-20 23:17:18 +0700

    navbar done, change icon hehe *isengg

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaBars } from 'react-icons/fa';
 import { HiX } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
+import './style.scss';
 
 const data = [
     { label: 'Home', to: '/' },
@@ -13,22 +14,24 @@ const data = [
 ];
 
 const Navbar = () => {
-    const [toggleIcon, setToggleIcon] = useState(false);
-    const handleToggleIcon = () => setToggleIcon(!toggleIcon);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const handleToggleMenu = () => {
+        setIsMobileMenuOpen((prev) => !prev);
+    };
 
     return (
-        <div>
-            <nav className="navbar">
-                <div className="navbar_container">
-                    <Link to='/' className="navbar_container_logo">
-                        <img
-                            src="/logo_portofolio_no_background.png"
-                            alt="JCE Logo"
-                            style={{ height: "40px" }}
-                        />
-                    </Link>
-                </div>
-                <ul className="navbar_container_menu">
+        <nav className="navbar">
+            <div className="navbar_container">
+                <Link to="/" className="navbar_container_logo">
+                    <img
+                        src="/logo_portofolio_animal_no_background.png"
+                        alt="JCE Logo"
+                        style={{ height: "60px" }}
+                    />
+                </Link>
+
+                <ul className={`navbar_container_menu ${isMobileMenuOpen ? 'active' : ''}`}>
                     {data.map((item, key) => (
                         <li key={key} className="navbar_container_menu_item">
                             <Link className="navbar_container_menu_item_links" to={item.to}>
@@ -37,11 +40,12 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
-                <div className="nav-icon" onClick={handleToggleIcon}>
-                    {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+
+                <div className="nav-icon" onClick={handleToggleMenu}>
+                    {isMobileMenuOpen ? <HiX size={30} /> : <FaBars size={30} />}
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 };
 
